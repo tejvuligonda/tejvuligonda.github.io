ca3a839f04b85c16308e50f48fc4c6c349c31798
tejvuligonda <tejvuligonda@gmail.com>
2017-09-14 23:28:16 +0000

    Added Ubuntu setup script for a server

---
+++ Ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y vim git tree curl
+if ( lspci | grep 'VirtualBox' >/dev/null );
+    then sudo apt-get install -y virtualbox-guest-dkms openssh-server;
+fi
+mkdir -p ~/Developer/public ~/Developer/private ~/Developer/test
+git clone https://github.com/tejvuligonda/OS-Starter-Packs.git ~/Developer/public/OS-Starter-Packs
